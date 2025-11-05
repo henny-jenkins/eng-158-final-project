6435507c50b1f635cd018972321e815d5b37b80f
Henry Jenkins <hjenkins2323@gmail.com>
2025-11-05 13:00:26 -0500

    incremental commit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-# from dynamics import dynamics_pd, dynamics_pd_pk
+from dynamics import dynamics_pd, dynamics_pd_pk
 from dataclasses import dataclass
 import matplotlib.pyplot as plt 
 from scipy.integrate import solve_ivp   # for the dynamics simulation
+from scipy.interpolate import interp1d
 import numpy as np
 
 @dataclass
@@ -24,7 +25,36 @@ class SimParams:
     rho: float | None       # drug clearance rate
 
 def main():
-    pass
+    # instantiate parameters for each cell population from Eastman et al. 2021
+    bone_marrow_params = CellPopulationParams(
+            gamma=1.470,
+            delta=0.000,
+            alpha=5.643,
+            beta=0.480,
+            lambda_param=0.164
+            )
+    breast_cancer_params = CellPopulationParams(
+            gamma=0.500,
+            delta=0.477,
+            alpha=0.218,
+            beta=0.050,
+            lambda_param=0.000
+            )
+    ovarian_cancer_params = CellPopulationParams(
+            gamma=0.6685,
+            delta=0.4597,
+            alpha=0.2225,
+            beta=0.0500,
+            lambda_param=0.0000
+            )
+
+    # define simulation configuration
+    SIMULATION_TIME = 21    # [days?]
+    SIM_STEPS = 1000
+    t_input = np.linspace(0, SIMULATION_TIME, SIM_STEPS)
+    u_input = np.sin(t_input)   # arbitrary control signal for now — would obtain from optimizer for MPC or policy for RL
+    u = interp1d(t_input, u_input, fill_value="extrapolate")
+    print("everything working so far")
 
 if __name__ == "__main__":
     main()
